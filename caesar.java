1df03e1208758536052b2415e5b41776a9b461d1
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-03 15:16:30 -0400

    progress with java

--- caesar.java
+++ caesar.java
@@ -10,3 +10,13 @@ String input_string = scan.nextLine();
 
 System.out.println("Now encrypting " + input_string + "...")
 
+String encrypted = "";
+
+for (int i = 0; i < input_string.length(); i++){
+    char c = input_string.charAt(i);
+    int position = alphabet.indexOf(c);
+    char new_char = cipher.charAt(position);
+    encrypted += new_char;
+}
+
+System.out.println("Encrypted string: "  + encrypted);
