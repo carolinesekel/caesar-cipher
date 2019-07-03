a2574be6cec0106c7b7d38b60ef00cc232883e4a
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-03 15:11:00 -0400

    starting java

---
+++ caesar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+String alphabet = "abcdefghijklmnopqrstuvwxyz";
+String cipher = "fghijklmnopqrstuvwxyzabcde";
+
+System.out.print("What do you want to encrypt? ");
+
+Scanner scan = new Scanner(System.in);
+String input_string = scan.nextLine();
+
+System.out.println("Now encrypting " + input_string + "...")
+
