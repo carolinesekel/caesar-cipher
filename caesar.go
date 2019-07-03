69acda8c77715dee2cc09d295563992be62d83d4
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-03 12:25:17 -0400

    beginning of go code

---
+++ caesar.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+func main() {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	//shift by 5
+	cipher := "fghijklmnopqrstuvwxyzabcde"
+	string input_string = 
+	for i:=0; i<len(input_string); i++ {
+		position := input_string.Index(alphabet, input_string[i])
+	}
+	
+
+
+}
