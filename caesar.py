a29ccc68fc3b1d048b9683f01c2d91bd3b94df6f
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-03 12:11:16 -0400

    python code

--- caesar.py
+++ caesar.py
@@ -1,1 +1,15 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+#shift by 5
+cipher = "fghijklmnopqrstuvwxyzabcde"
+
 input_string = input("What would you like to encrypt? ")
+print("Now encrypting ", input_string, "...")
+
+encrypted = ""
+
+for letter in input_string:
+    position = alphabet.index(letter)
+    new_char = cipher[position]
+    encrypted += new_char
+
+print("Encrypted string: ", encrypted)
