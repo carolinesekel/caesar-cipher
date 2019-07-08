878ac18eec5513ba0c0933f24c565a2ac1200510
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-08 10:28:45 -0400

    removed comments from testing

--- caesar.lua
+++ caesar.lua
@@ -1,4 +1,3 @@
---print("Hello World!")
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 --shift by 5
 cipher = "fghijklmnopqrstuvwxyzabcde"
@@ -7,11 +6,8 @@ io.write("What would you lke to encrypt?\n")
 input_string=io.read()
 encrypted = ''
 for c in input_string:gmatch"." do
-    --print(c)
     position = string.find(alphabet, c)
-    --print(position)
     new_char = cipher:sub(position,position)
-    --print(new_char)
     encrypted = encrypted .. new_char
 end
 print("Encrypted string: ")
